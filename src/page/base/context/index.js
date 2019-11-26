5a43d7da34e4f1616de73b6c8764b9be1f5c6889
haiyang.li <haiyang.li@asiainnovations.com>
2019-11-26 18:22:51 +0800

    context

--- src/page/base/context/index.js
+++ src/page/base/context/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import mod1 from './mod1';
+import Mod1 from './mod1';
+import Mod2 from './mod2';
 
 export default class View extends React.Component {
     constructor(props) {
@@ -10,7 +10,8 @@ export default class View extends React.Component {
     render() {
         return (
             <div className="context-warpper">
-                <Route path="/context/mod1" component={mod1} />
+                <Mod1 />
+                <Mod2 />
             </div>
         );
     }
--- src/page/base/index.js
+++ src/page/base/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import context from './context';
+import {Route} from 'react-router-dom';
 
 export default () => {
-    return <React.Fragment>1111</React.Fragment>
+    return (
+    <React.Fragment>
+        <Route path="/base/context" component={context} />
+    </React.Fragment>
+    )
 }
