9e196a128d7bc2882b99f52b218e1bfc098c365c
haiyang.li <haiyang.li@asiainnovations.com>
2019-12-04 19:12:55 +0800

    调整

--- src/component/page.js
+++ src/component/page.js
@@ -4,6 +4,6 @@ import React from 'react';
 export default (props) => {
     const {className = '', children, ...other} = props;
     return (
-    <div className={`page-wrapper ${className}`}>{children}</div>
+        <div className={`page-wrapper ${className}`}>{children}</div>
     )
 }
