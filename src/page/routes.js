9817a31ee42e286818d89085e71bbb0b69bb57dd
haiyang.li <haiyang.li@asiainnovations.com>
2020-02-24 18:53:32 +0800

    update

--- src/page/routes.js
+++ src/page/routes.js
@@ -3,7 +3,6 @@ import { Route } from 'react-router-dom';
 // layout
 import base from './base';
 import scene from './scene';
-import hooks from './hooks';
 import hoc from './hoc';
 import store from './store';
 
@@ -16,7 +15,6 @@ export default class View extends React.Component {
                 <Route path="/base" component={base} />
                 <Route path="/scene" component={scene} />
                 <Route path="/hoc" component={hoc} />
-                <Route path="/hooks" component={hooks} />
                 <Route path="/store" component={store} />
             </React.Fragment>
         );
--- src/page/store/index.js
+++ src/page/store/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 // layout
-import flux from './flux';
 import common from './common';
 import mobx from './mobx';
 import redux from './redux';
@@ -10,7 +9,6 @@ export default () => {
     return (
         <React.Fragment>
             <Route path="/store/common" component={common} />
-            <Route path="/store/flux" component={flux} />
             <Route path="/store/mobx" component={mobx} />
             <Route path="/store/redux" component={redux} />
         </React.Fragment>
